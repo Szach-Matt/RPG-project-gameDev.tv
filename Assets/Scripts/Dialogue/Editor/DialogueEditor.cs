063ed4183370dc0f0930bf246d07803af6aedae4
Matt <trotrex91@hotmail.com>
2023-03-02 01:08:39 +0100

    Add node display system into the DialogueEditor

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,10 +1,13 @@
 using UnityEditor;
 using UnityEditor.Callbacks;
+using UnityEngine;
 
 namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        Dialogue selectedDialogue = null;
+        GUIStyle nodeStyle;
 
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
@@ -12,17 +15,71 @@ namespace RPG.Dialogue.Editor
             GetWindow(typeof(DialogueEditor), false, "DialogueEditor");
         }
 
-        [OnOpenAssetAttribute(1)]
+        [OnOpenAsset(1)]
         public static bool OnOpenAsset(int instaceID, int line)
         {
 
-            var obj = EditorUtility.InstanceIDToObject(instaceID) as Dialogue;
-            if (obj != null)
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instaceID) as Dialogue;
+            if (dialogue != null)
             {
                 ShowEditorWindow();
                 return true;
             }
             return false;
+        }
+
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+
+            nodeStyle = new GUIStyle();
+            nodeStyle.normal.background = EditorGUIUtility.Load("node0") as Texture2D;
+            nodeStyle.padding = new RectOffset(20, 20, 20, 20);
+            nodeStyle.border = new RectOffset(10, 10, 10, 10);
+        }
+
+        private void OnSelectionChanged()
+        {
+            Dialogue newDialogue = Selection.activeObject as Dialogue;
+            if (newDialogue != null)
+            {
+                selectedDialogue = newDialogue;
+                Repaint();
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected.");
+            }
+            else
+            {
+                foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+                {
+                    OnGUINode(node);
+                }
+            }
+        }
+
+        private void OnGUINode(DialogueNode node)
+        {
+            GUILayout.BeginArea(node.position, nodeStyle);
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUILayout.LabelField("Node:", EditorStyles.whiteLabel);
+            string newText = EditorGUILayout.TextField(node.text);
+            string newID = EditorGUILayout.TextField(node.uniqueID);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(selectedDialogue, "Update Dialogue Text");
+
+                node.text = newText;
+                node.uniqueID = newID;
+            }
+            GUILayout.EndArea();
         }
     }
 }
